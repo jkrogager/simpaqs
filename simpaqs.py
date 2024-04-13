32774d7383f1a91ee041e3e668d2b16e41f16303
Jens-Kristian Krogager <jens-kristian.krogager@univ-lyon1.fr>
2024-04-13 19:57:59 -0400

    Included simulation of BALs into simpaqs
    There are three types of BAL models, felobal, hibal_1, and hibal_2.
    For any given quasar, a random model is chosen with 20% chance each,
    the remaining 40% chance is a non-BAL quasar. This gives a fair mix,
    though not representative!

--- simpaqs.py
+++ simpaqs.py
@@ -1,11 +1,11 @@
 
 import sys
+import numpy as np
+from astropy import table
 
 from simulate_absorbers import make_absorber_templates
 from simulate_quasars import add_quasar_continuum
 from simulate_spectra import process_catalog
-import numpy as np
-from astropy import table
 
 __author__ = 'Jens-Kristian Krogager'
 __email__ = 'jens-kristian.krogager@univ-lyon1.fr'
@@ -22,13 +22,13 @@ OUTPUT_DIR = 'paqs'
 ##########################
 
 N_TOTAL = int(sys.argv[1])
-np.random.seed(20230521)
+# np.random.seed(20230521)
 
 # abs_template_list, abslog, DLAlog = make_absorber_templates(N_TOTAL, z_min=Z_MIN, z_max=Z_MAX, verbose=True)
 
 abs_template_list = table.Table.read("output/list_templates.csv") # For midway inspection
 
-model_input = add_quasar_continuum(abs_template_list)
+model_input = add_quasar_continuum(abs_template_list, BAL=True, output_dir='output/BAL_models')
 
 #model_input = table.Table.read('output/quasar_models/model_input.csv') # For midway inspection
 
