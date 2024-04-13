7aa0dcd689b2607db2fa165ef4f0cdf2eb7ae606
Jens-Kristian Krogager <jens-kristian.krogager@univ-lyon1.fr>
2024-04-13 18:55:44 -0400

    Updated to simulate subset of PAQS catalog
    Using simulated spectra from SIMPAQS, generate a subset of the PAQS
    catalog to run through the simulate_catalog.py

--- simpaqs.py
+++ simpaqs.py
@@ -12,26 +12,26 @@ __email__ = 'jens-kristian.krogager@univ-lyon1.fr'
 
 
 # -- Input parameters
-Z_MIN = 2
+Z_MIN = 0.5
 Z_MAX = 4
 EXPTIME = 3600  # seconds
 MOON = 'dark'
 MAG_MIN = 18
 MAG_MAX = 20.5
-OUTPUT_DIR = 'output/l1_data'
+OUTPUT_DIR = 'paqs'
 ##########################
 
 N_TOTAL = int(sys.argv[1])
 np.random.seed(20230521)
 
-abs_template_list, abslog, DLAlog = make_absorber_templates(N_TOTAL, z_min=Z_MIN, z_max=Z_MAX, verbose=True)
+# abs_template_list, abslog, DLAlog = make_absorber_templates(N_TOTAL, z_min=Z_MIN, z_max=Z_MAX, verbose=True)
 
-#abs_template_list = table.Table.read("output/list_templates.csv") # For midway inspection
+abs_template_list = table.Table.read("output/list_templates.csv") # For midway inspection
 
 model_input = add_quasar_continuum(abs_template_list)
 
 #model_input = table.Table.read('output/quasar_models/model_input.csv') # For midway inspection
 
-process_catalog(model_input, mag_min=MAG_MIN, mag_max=MAG_MAX,
-                exptime=EXPTIME, moon=MOON, output=OUTPUT_DIR)
+# process_catalog(model_input, mag_min=MAG_MIN, mag_max=MAG_MAX,
+#                 exptime=EXPTIME, moon=MOON, output=OUTPUT_DIR)
 
