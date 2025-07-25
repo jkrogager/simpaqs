b99a606208c85033e1221c58bf3e2c36841fe7ed
Jens-Kristian Krogager <jens-kristian.krogager@univ-lyon1.fr>
2025-07-24 22:31:10 -0400

    Migrated to new ETC written by JK

--- simpaqs.py
+++ simpaqs.py
@@ -18,19 +18,19 @@ EXPTIME = 3000  # seconds
 MOON = 'dark'
 MAG_MIN = 19
 MAG_MAX = 20.5
-OUTPUT_DIR = 'output/l1_data'
-ABS_MODELS_DIR = 'output/abs'
-QSO_MODELS_DIR = 'output/quasars'
+OUTPUT_DIR = 'test/l1_data'
+ABS_MODELS_DIR = 'test/abs'
+QSO_MODELS_DIR = 'test/quasars'
 BAL = False
 ##########################
 
 N_TOTAL = int(sys.argv[1])
 # np.random.seed(20230521)
 
-abs_template_list, abslog, DLAlog = make_absorber_templates(N_TOTAL, z_min=Z_MIN, z_max=Z_MAX, verbose=True,
-                                                            output_dir=ABS_MODELS_DIR)
+# abs_template_list, abslog, DLAlog = make_absorber_templates(N_TOTAL, z_min=Z_MIN, z_max=Z_MAX, verbose=True,
+#                                                             output_dir=ABS_MODELS_DIR)
 
-# abs_template_list = table.Table.read("output/list_templates.csv") # For midway inspection
+abs_template_list = table.Table.read("test/abs/list_templates.csv") # For midway inspection
 
 model_input = add_quasar_continuum(abs_template_list, BAL=BAL, output_dir=QSO_MODELS_DIR,
                                    # dust_mode='flat',
@@ -40,4 +40,3 @@ model_input = add_quasar_continuum(abs_template_list, BAL=BAL, output_dir=QSO_MO
 
 process_catalog(model_input, mag_min=MAG_MIN, mag_max=MAG_MAX, template_path=QSO_MODELS_DIR,
                 exptime=EXPTIME, moon=MOON, output=OUTPUT_DIR)
-
